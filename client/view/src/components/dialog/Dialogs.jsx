143efd4d695a41d0c6895e4756032371d006ce33
HieuMax <118246782+HieuMax@users.noreply.github.com>
2024-11-26 02:11:32 +0700

    Update Dialogs.jsx

--- client/view/src/components/dialog/Dialogs.jsx
+++ client/view/src/components/dialog/Dialogs.jsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { useCallback, useEffect, useState } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 import { UserPlus } from 'lucide-react'
